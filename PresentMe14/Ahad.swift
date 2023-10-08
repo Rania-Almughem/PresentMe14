b668d023bd4ff61b638250c60e6e0fc774582993
Ahad <ahoudtiion@hotmail.com>
2023-10-08 12:27:16 +0300

    Ahad

---
+++ PresentMe14/Ahad.swift
@@ -0,0 +1,18 @@
+//
+//  Ahad.swift
+//  PresentMe14
+//
+//  Created by Ahad on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Ahad: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ahad()
+}
