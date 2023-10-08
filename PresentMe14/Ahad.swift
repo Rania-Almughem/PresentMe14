7cac0c8cf3662fdf0ac3c3d9c197642a7aefba57
Ahad <ahoudtiion@hotmail.com>
2023-10-08 12:27:40 +0300

    Ahad

--- PresentMe14/Ahad.swift
+++ PresentMe14/Ahad.swift
@@ -1,17 +1,40 @@
-//
-//  Ahad.swift
-//  PresentMe14
-//
-//  Created by Ahad on 23/03/1445 AH.
-//
 
 import SwiftUI
 
 struct Ahad: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            Image("Image2")
+                .foregroundColor(Color.black)
+                .clipShape(Circle())
+                .cornerRadius(7)
+                .padding()
+                .background(Color.black)
+                .border(Color.black, width: 10)
+
+                .cornerRadius(250)
+            
+            Text("Ahad Alfarraj")
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Text("Management Information Systems fresh graduate, I’m 23 years old, I’m a learner at Apple Developer Academy & happy and excited for this journey.")
+                .multilineTextAlignment(.center)
+                
+            
+            
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all, 30.0/*@END_MENU_TOKEN@*/)
+        
+        
+        
+  
+        }
+     
     }
-}
+
 
 #Preview {
     Ahad()
