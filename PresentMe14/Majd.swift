0897e1e3efe11dbd0a65b45297f8f89b9914bd01
Majd <llmajdll22@gmail.com>
2023-10-08 12:46:07 +0300

    majd

---
+++ PresentMe14/Majd.swift
@@ -0,0 +1,18 @@
+//
+//  Majd.swift
+//  PresentMe14
+//
+//  Created by Majd Abdullah on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Majd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Majd()
+}
