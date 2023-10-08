25207aafd86bb4014137140afabd0c811667cd71
Majd <llmajdll22@gmail.com>
2023-10-08 12:46:36 +0300

    majd

--- PresentMe14/Majd.swift
+++ PresentMe14/Majd.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct Majd: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+        
+            Color.color
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                
+                Image("3")
+                    .resizable()
+                    .clipShape(Circle())
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300)
+                .shadow(radius: 10.0)
+                
+              Text("MAJD ABDULLAH ❤︎")
+                    .foregroundColor(.white)
+                    .italic()
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    
+                    
+                Text("She wants to accomplish something in her life, learn a new languages, learn new things, see the world, read a thousand books, she wants to discover the essence, whatever that word may mean, she wants to find out if there's any essence at all..")
+                    .foregroundColor(.black)
+                    .font(.system(size: 15))
+                    .padding()
+            
+            }
+       
+        }
     }
 }
 
