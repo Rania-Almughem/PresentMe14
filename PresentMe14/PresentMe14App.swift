0255505c7a1f1fd02d95fa7bcbd1c1b3023e2c48
rania-almughem <rania.almughem@gmail.com>
2023-10-08 12:15:04 +0300

    Initial Commit

---
+++ PresentMe14/PresentMe14App.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMe14App.swift
+//  PresentMe14
+//
+//  Created by rania on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMe14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
