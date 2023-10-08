e08879aced77ea74b8ac0dafdc99fc057c3a5cdb
rania-almughem <rania.almughem@gmail.com>
2023-10-08 12:17:29 +0300

    rania

--- PresentMe14/ContentView.swift
+++ PresentMe14/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            
+            VStack{
+                Image("1")
+                    .resizable()
+                    .frame(width: 200.0, height: 200.0)
+                    .cornerRadius(100)
+                
+                
+                Text("Rania Almughem")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .padding()
+                Text("Computer science graduate, love learn about various fields, programming, networks and databases. I hope to achieve my dreams and transform myself to be the best version of myself🩷.")
+                    .multilineTextAlignment(.center)
+                
+                    .padding()
+            }
+            
+            
+            
         }
-        .padding()
     }
 }
 
